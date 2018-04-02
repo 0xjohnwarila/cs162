c175e4647ed22f94bc2b7d8ec6bef4bbb7426238
John Warila <johnwarila@gmail.com>
2018-04-02 16:19:16 -0700

    Stores the maze in a vector of strings, you can look at lines, or at individual points in the line.

--- mazes/mazeOUT.cpp
+++ mazes/mazeOUT.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	using std::cout;
 	using std::endl;
 	using std::cin;
 	using std::string;
 	using std::ifstream;
+	using std::vector;
 
 	ifstream in(argv[1]);
 
-	string str[1024];
+	string line;
+	vector<string> lines;
 	int i = 0;
 
-	while(getline(in, str[i])){
-		cout << str[i] << endl;
+	while(getline(in, line)) {
+		lines.push_back(line);
 		i++;
 	}
 
-	
+	for(int x = 0; x <= i; x++){
+		cout << lines[x] << endl;
+	}
+
 	return 0;
 }
